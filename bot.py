4f524a3a8502ca10033c9f15d5be74e14f7a636f
nikitakosatka <nikitakosatka@yandex.ru>
2019-05-02 22:48:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,85 +1,133 @@
-# Импортируем необходимые классы.
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-from datetime import datetime
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler
+from random import shuffle
 import json
 
 with open("test.json", "rt", encoding="utf8") as f:
     test = json.loads(f.read())['test']
-print(test)
 
-# Напишем соответствующие функции.
-# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
+
 def start(bot, update):
-    updater = Updater("845010139:AAEtih9AAKtMBVRc0kvrmVAeQ712l9EYyhg")
-    test.reverse()
+    shuffle(test)
     update.message.reply_text(
-        "Привет! Сейчас будет тест по истории!"
-    )
-    updater.start_polling()
-    update.message.reply_text("Привет")
+        "Привет. Это тест по истории, состоящий из 6 перемешанных вопросов\n"
+        "Вы можете прервать тест, послав команду /stop.\n"
+        "{}".format(test[0]['question']))
+
+    return 1
 
 
+def first_response(bot, update, user_data):
+    if update.message.text != test[0]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[0]["response"]}')
+        user_data['right_answers'] = 0
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] = 1
 
-def help(bot, update):
     update.message.reply_text(
-        "Я пока не умею помогать... Я только ваше эхо.")
+        "{}".format(test[1]['question']))
+    return 2
 
 
-def time(bot, update):
-    update.message.reply_text(datetime.strftime(datetime.now(), "%H:%M:%S"))
+def second_response(bot, update, user_data):
+    if update.message.text != test[1]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[1]["response"]}')
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] += 1
+
+    update.message.reply_text(
+        "{}".format(test[2]['question']))
+    return 3
 
 
-def date(bot, update):
-    update.message.reply_text(datetime.strftime(datetime.now(), "%x"))
+def third_response(bot, update, user_data):
+    if update.message.text != test[2]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[2]["response"]}')
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] += 1
+
+    update.message.reply_text(
+        "{}".format(test[3]['question']))
+    return 4
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, сам бот и класс updater, принявший сообщение.
-def echo(bot, update):
-    # У объекта класса Updater есть поле message, являющееся
-    # объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str), отсылающий ответ пользователю,
-    # от которого получено сообщение.
-    text = list(update.message.text)
-    text.reverse()
-    update.message.reply_text(''.join(text))
+def fourth_response(bot, update, user_data):
+    if update.message.text != test[3]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[3]["response"]}')
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] += 1
+
+    update.message.reply_text(
+        "{}".format(test[4]['question']))
+    return 5
+
+
+def fifth_response(bot, update, user_data):
+    if update.message.text != test[4]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[4]["response"]}')
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] += 1
+
+    update.message.reply_text(
+        "{}".format(test[5]['question']))
+    return 6
+
+
+def sixth_response(bot, update, user_data):
+    if update.message.text != test[5]['response']:
+        update.message.reply_text(f'Неверно. Правильный ответ: {test[5]["response"]}')
+    else:
+        update.message.reply_text('Верно!')
+        user_data['right_answers'] += 1
+    print(user_data['right_answers'])
+    update.message.reply_text(
+        'Конец теста. Количество правильных ответов - {}/6'.format(user_data["right_answers"]))
+    return ConversationHandler.END
+
+
+def stop(bot, update):
+    update.message.reply_text(
+        "Ну ладно")
+    return ConversationHandler.END
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+
+    states={
+        # Добавили user_data для сохранения ответа.
+        1: [MessageHandler(Filters.text, first_response,
+                           pass_user_data=True)],
+        # ...и для его использования.
+        2: [MessageHandler(Filters.text, second_response,
+                           pass_user_data=True)],
+        3: [MessageHandler(Filters.text, third_response,
+                           pass_user_data=True)],
+        4: [MessageHandler(Filters.text, fourth_response,
+                           pass_user_data=True)],
+        5: [MessageHandler(Filters.text, fifth_response,
+                           pass_user_data=True)],
+        6: [MessageHandler(Filters.text, sixth_response,
+                           pass_user_data=True)]
+    },
+
+    fallbacks=[CommandHandler('stop', stop)]
+)
 
 
 def main():
-    # Создаём объект updater. Вместо слова "TOKEN" надо разместить
-    # полученный от @BotFather токен
     updater = Updater("845010139:AAEtih9AAKtMBVRc0kvrmVAeQ712l9EYyhg")
 
-    # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
+    dp.add_handler(conv_handler)
 
-    # Зарегистрируем их в диспетчере рядом с регистрацией
-    # обработчиков текстовых сообщений.
-    # Первым параметром конструктора CommandHandler является название команды.
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("time", time))
-    dp.add_handler(CommandHandler("date", date))
-
-    # Создаём обработчик сообщений типа Filters.text
-    # из описанной выше функции echo()
-    # После регистрации обработчика в диспетчере эта функция
-    # будет вызываться при получении сообщения с типом "текст",
-    # т.е. текстовых сообщений.
-    text_handler = MessageHandler(Filters.text, echo)
-
-    # Регистрируем обработчик в диспетчере.
-    dp.add_handler(text_handler)
-
-    # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
-
-    # Ждём завершения приложения.
-    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
